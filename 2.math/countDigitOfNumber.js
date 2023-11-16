4cb4d0552f6a1624ea0a706cebeb47d9aa9655c3
jaswinder Kumar <kumarsanju54@gmail.com>
2023-11-16 13:22:59 +0000

    count numbers

---
+++ 2.math/countDigitOfNumber.js
@@ -0,0 +1,12 @@
+let num = 12398;
+let x = 0;
+function countNum(num){
+  
+    while(num != 0){
+        num = Math.floor(num/10); 
+        x++
+    }
+
+    return num
+}
+console.log(countNum(num), x);
